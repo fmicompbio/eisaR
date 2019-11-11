1c55aba90238f1a91a4a3b7390a244de02da5aad
mbstadler <stadler.michael@gmail.com>
2019-11-11 14:31:56 +0100

    remove transparency from plotEISA

--- R/plotEISA.R
+++ R/plotEISA.R
@@ -16,7 +16,13 @@
 #'   highlighting.
 #' @param genecolors Vector of length three specifying the colors to use for
 #'   genes that are significantly up, down or unchanged.
-#' @param ... further arguments past to \code{plot()}
+#' @param ... further arguments past to \code{plot()}. Parameters that will be set
+#'   automatically unless given in the arguments are:\describe{
+#'     \item{pch}{: plot symbol (default: \code{"."})}
+#'     \item{cex}{: plot symbol expansion factor (default: \code{2})}
+#'     \item{col}{: plot symbol color (default: according to \code{contrast} and \code{genecolors})}
+#'     \item{xlab/ylab}{: axis labels}
+#'   }
 #'
 #' @return `NULL` (invisibly)
 #'
@@ -28,9 +34,10 @@
 #' @export
 plotEISA <- function(x, contrast = c("ExIn", "none"),
                      minLfc = NULL, maxFDR = 0.05,
-                     genecolors = c("#E41A1CFF", "#4DAF4AFF", "#22222244"), ...) {
+                     genecolors = c("#E41A1C", "#4DAF4A", "#222222"), ...) {
     # check arguments
     contrast <- match.arg(contrast)
+    contrastName <- ifelse("contrastName" %in% names(x), paste0(" (",x$contrastName, ")"), "")
     sigtab <- switch(contrast, ExIn = x$tab.ExIn, none = data.frame())
     if (nrow(sigtab) == 0 && contrast != "none")
         stop("'x' does not contain the requested statistics and can only ",
@@ -65,17 +72,22 @@ plotEISA <- function(x, contrast = c("ExIn", "none"),
     dotsL$x <- x$contrasts[, "Din"]
     dotsL$y <- x$contrasts[, "Dex"]
     if (!"pch" %in% names(dotsL))
-        dotsL$pch <- 20
+        dotsL$pch <- "."
+    if (!"cex" %in% names(dotsL))
+        dotsL$cex <- 2
     if (!"col" %in% names(dotsL))
         dotsL$col <- ifelse(sig, ifelse(sigtab$logFC > 0, genecolors[1], genecolors[2]), genecolors[3])
     if (!"xlab" %in% names(dotsL))
-        dotsL$xlab <- substitute(expression(paste(Delta, "intron (", cn, ")")), list(cn = x$contrastName))
+        dotsL$xlab <- substitute(expression(paste(Delta, "intron", cn)), list(cn = contrastName))
     if (!"ylab" %in% names(dotsL))
-        dotsL$ylab <- substitute(expression(paste(Delta, "exon (", cn, ")")), list(cn = x$contrastName))
+        dotsL$ylab <- substitute(expression(paste(Delta, "exon", cn)), list(cn = contrastName))
 
     # Delta I vs. Delta E
     do.call(plot, dotsL)
     if (contrast != "none") {
+        if (length(dotsL$col) == 1L)
+            dotsL$col <- rep(dotsL$col, length(dotsL$x))
+        points(dotsL$x[sig], dotsL$y[sig], pch = 20, col = dotsL$col[sig])
         legend(x = "bottomright", bty = "n", pch = 20, col = genecolors[1:2],
                legend = sprintf("%s (%d)", c("Up","Down"), c(sum(sig.dir == 1), sum(sig.dir == -1))))
     }
